6cd42df1c34b2afcb5219082589baaa183b59d8a
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2023-03-07 19:10:58 -0300

    feat: add tests for validateInputs fn

--- javascript/customer-success-balancing.js
+++ javascript/customer-success-balancing.js
@@ -4,6 +4,30 @@
  * @param {array} customers
  * @param {array} customerSuccessAway
  */
+
+const ValidationErrors = {
+  CS_LENGTH: "number of CSs allowed",
+  CS_AWAY: "number of CSs away allowed",
+  CS_ALLOWED_IDS: "value to CS ID",
+  CS_ALLOWED_SCORES: "value to CS score",
+  CS_DIFFERENT_SCORES: "scores of CSs, they should be different",
+  CUSTOMER_LENGTH: "number of customers",
+  CUSTOMER_ALLOWED_ID: "value to customer ID",
+  CUSTOMER_ALLOWED_SCORE: "value to customer score",
+};
+
+const maxCsLength = 1000;
+const maxCustomersLength = 1000000;
+const maxCsId = 1000;
+const maxCsScore = 10000;
+const maxCustomerId = 1000000;
+const maxCustomerScore = 100000;
+
+function getMaxCsAway(customerSuccess) {
+  const n = customerSuccess.length;
+  return Math.floor(n / 2);
+}
+
 function customerSuccessBalancing(
   customerSuccess,
   customers,
@@ -108,63 +132,73 @@ function validateInputs(customerSuccess, customers, customerSuccessAway) {
   const n = customerSuccess.length;
   const m = customers.length;
   const t = customerSuccessAway.length;
-  const csScores = customerSuccess.map((cs) => cs.score);
 
-  const maxCsLength = 1000;
-  const maxCustomersLength = 1000000;
-  const maxCsId = 1000;
-  const maxCsScore = 10000;
-  const maxCustomerId = 1000000;
-  const maxCustomerScore = 100000;
-  const maxCsAway = Math.floor(n / 2);
+  const csScores = customerSuccess.map((cs) => cs.score);
+  const csIds = customerSuccess.map((cs) => cs.id);
+
+  const customersIds = customers.map((c) => c.id);
+  const customersScores = customers.map((c) => c.score);
 
   const inputValidations = [
     {
-      name: "number of CSs",
+      name: ValidationErrors.CS_LENGTH,
       value: n,
       max: maxCsLength,
     },
     {
-      name: "number of customers",
+      name: ValidationErrors.CUSTOMER_LENGTH,
       value: m,
       max: maxCustomersLength,
     },
     {
-      name: "number of CSs away",
+      name: ValidationErrors.CS_AWAY,
       value: t,
-      max: maxCsAway,
+      max: getMaxCsAway(customerSuccess),
     },
     {
-      name: "CS IDs",
-      value: customerSuccess.map((cs) => cs.id),
+      name: ValidationErrors.CS_ALLOWED_IDS,
+      value: csIds,
       max: maxCsId,
     },
     {
-      name: "CS scores",
+      name: ValidationErrors.CS_ALLOWED_SCORES,
       value: csScores,
       max: maxCsScore,
     },
     {
-      name: "customer IDs",
-      value: customers.map((c) => c.id),
+      name: ValidationErrors.CUSTOMER_ALLOWED_ID,
+      value: customersIds,
       max: maxCustomerId,
     },
     {
-      name: "customer scores",
-      value: customers.map((c) => c.score),
+      name: ValidationErrors.CUSTOMER_ALLOWED_SCORE,
+      value: customersScores,
       max: maxCustomerScore,
     },
   ];
 
   if (new Set(csScores).size !== csScores.length) {
-    throw new Error("All CSs should have different levels.");
+    throw new Error(
+      `Invalid ${ValidationErrors.CS_DIFFERENT_SCORES}. All CSs should have different levels.`
+    );
   }
 
   for (const { name, value, max } of inputValidations) {
-    if (name == "number of CSs away") {
-      if (t > maxCsAway) {
+    if (name === ValidationErrors.CS_AWAY) {
+      if (t > getMaxCsAway(customerSuccess)) {
         throw new Error(
-          `Invalid number of CSs away: ${t}. The number of CSs away must be less than ${maxCsAway}.`
+          `Invalid ${
+            ValidationErrors.CS_AWAY
+          }: ${t}. The number of CSs away must be less than ${getMaxCsAway(
+            customerSuccess
+          )}.`
+        );
+      }
+    } else if (Array.isArray(value)) {
+      const hasInvalidValues = value.some((v) => v <= 0 || v >= max);
+      if (hasInvalidValues) {
+        throw new Error(
+          `Invalid ${name}: ${value}. The ${name} must be greater than 0 and less than ${max}.`
         );
       }
     } else if (value <= 0 || value >= max) {
@@ -230,4 +264,100 @@ test("Scenario 7", () => {
   const csAway = [4, 5, 6];
 
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(3);
+});
+
+describe("Scenario 8 - validateInputs", () => {
+  it("throws an error if all CSs do not have different scores", () => {
+    const customerSuccess = [
+      { id: 1, score: 100 },
+      { id: 2, score: 100 },
+    ];
+    const customers = [];
+    const customerSuccessAway = [];
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CS_DIFFERENT_SCORES));
+  });
+
+  it(`throws an error if the number of CSs is equal or greater than ${maxCsLength}`, () => {
+    const customerSuccess = mapEntities(arraySeq(maxCsLength, 1));
+    const customers = [];
+    const customerSuccessAway = [];
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CS_LENGTH));
+  });
+
+  it(`throws an error if the number of customers is equal or greater than ${maxCustomersLength}`, () => {
+    const customerSuccess = mapEntities(arraySeq(2, 1));
+    const customers = Array(maxCustomersLength).fill(0);
+    const customerSuccessAway = [];
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CUSTOMER_LENGTH));
+  });
+
+  const customerSuccess = mapEntities(arraySeq(3, 1));
+  it(`throws an error if the number of CSs away is equal or greater than ${getMaxCsAway(
+    customerSuccess
+  )}`, () => {
+    const customers = mapEntities([3, 4, 5]);
+    const customerSuccessAway = [1, 2, 3];
+
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CS_AWAY));
+  });
+
+  it(`throws an error if the Cs ID is equal or greater than ${maxCsId}`, () => {
+    const customerSuccess = [
+      { id: maxCsId - 1, score: 10 },
+      { id: maxCsId, score: 20 },
+    ];
+    const customers = mapEntities([3, 4, 5]);
+    const customerSuccessAway = [];
+
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CS_ALLOWED_IDS));
+  });
+
+  it(`throws an error if the Cs score is equal or greater than ${maxCsScore}`, () => {
+    const customerSuccess = [
+      { id: 1, score: maxCsScore - 1 },
+      { id: 2, score: maxCsScore },
+    ];
+    const customers = mapEntities([3, 4, 5]);
+    const customerSuccessAway = [];
+
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CS_ALLOWED_SCORES));
+  });
+
+  it(`throws an error if the customer ID is equal or greater than ${maxCustomerId}`, () => {
+    const customerSuccess = mapEntities(arraySeq(2, 1));
+    const customers = [
+      { id: maxCustomerId - 1, score: 10 },
+      { id: maxCustomerId, score: 20 },
+    ];
+    const customerSuccessAway = [];
+
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CUSTOMER_ALLOWED_IDS));
+  });
+
+  it(`throws an error if the customer score is equal or greater than ${maxCustomerScore}`, () => {
+    const customerSuccess = mapEntities(arraySeq(2, 1));
+    const customers = [
+      { id: 1, score: maxCustomerScore - 1 },
+      { id: 2, score: maxCustomerScore },
+    ];
+    const customerSuccessAway = [];
+
+    expect(() =>
+      validateInputs(customerSuccess, customers, customerSuccessAway)
+    ).toThrowError(new RegExp(ValidationErrors.CUSTOMER_ALLOWED_SCORES));
+  });
 });
